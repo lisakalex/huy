9e784936dc7953c23e1bd8270f0b39698b768f88
al <lisakalex@gmail.com>
2023-05-17 20:20:01 +0100

    calendar

--- a/editallfiles.py
+++ a/editallfiles.py
@@ -1,5 +1,7 @@
 import glob2
+import shutil
 
+shutil.copytree('a/cryptonews.com', './', dirs_exist_ok=True)
 # this file must be in top directory where all files to be edited
 for filepath in glob2.iglob('./**/*.html', recursive=True):
     with open(filepath) as file:
--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,7 @@
 """
 process bank file, do not forget to change month and year
 """
-month = "Mar 23"
+month = "Apr 23"
 try:
     with open("bank.txt", "r", encoding='utf-8') as hf:
         bank = hf.read()
